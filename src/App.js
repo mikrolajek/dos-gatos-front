b626aac251a605f9ca5882a776d0ec9f53da26da
predotakinga <56541567+predotakinga@users.noreply.github.com>
2021-11-04 15:34:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     <>
     <Helmet>
       <title>Dos Gatos</title>
-      <link rel="icon" type="image/png" href={favicon} sizes="16x16" alt="favicon"/>
+      <link rel="icon" type="image/x-icon" href={favicon} sizes="16x16" alt="favicon"/>
       <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
       <meta name="HandheldFriendly" content="true" />
